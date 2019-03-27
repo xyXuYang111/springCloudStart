7a5a3e80120a1a723b1632a7a2a7d648cca0f8c0
xuy <1234qwer>
2019-03-28 00:23:04 +0800

    consul集成

--- feign/src/main/java/com/xuyang/springcloud/feign/feign/TestFeign.java
+++ feign/src/main/java/com/xuyang/springcloud/feign/feign/TestFeign.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @Auther: xuy
